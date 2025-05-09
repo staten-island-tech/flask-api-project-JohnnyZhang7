2c77941edf954c496a54df116528f0c4563b4f2b
JohnnyZhang7 <zjohnnyzhang@gmail.com>
2025-05-09 09:25:02 -0400

    lowkey cooking 5/9/25

--- app.py
+++ app.py
@@ -5,6 +5,37 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    response = requests.get("https://api.disneyapi.dev/character")
-    data = response.json()
-    print(data)
+    response = requests.get("https://www.freetogame.com/api/games?platform=pc")
+    data = response.json()[:50]
+
+    games = []
+
+    for game in data:
+        games.append({
+        'id': game['id'],
+        'name': game['title'],
+        'thumbnail': game['thumbnail'],
+        'genre': game['genre'],
+        'publisher': game['publisher']
+        })
+
+    return render_template("index.html", games=games)
+
+@app.route("/game/<int:game_id>")
+def description_link(game_id):
+    response = requests.get(f"https://www.freetogame.com/api/game/{game_id}")
+    game = response.json()
+
+    information = {
+        'name': game['title'],
+        'description': game['description'],
+        'url': game['freetogame_profileurl'],
+        'thumbnail': game['thumbnail'],
+        'genre': game['genre'],
+        'publisher': game['publisher']
+    }
+
+    return render_template("description.html", information=information)
+
+if __name__ == '__main__':
+    app.run(debug=True)
