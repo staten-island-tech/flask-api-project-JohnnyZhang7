37403fe92c2567c6cd6d51ed8338ff73335744df
JohnnyZhang7 <zjohnnyzhang@gmail.com>
2025-05-12 09:23:30 -0400

    5/12/25 cooking streak, add the url link next

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     response = requests.get("https://www.freetogame.com/api/games?platform=pc")
-    data = response.json()[:50]
+    data = response.json()[:125]
 
     games = []
 
@@ -23,20 +23,19 @@ def index():
 
 @app.route("/game/<int:game_id>")
 def description_link(game_id):
-    response = requests.get(f"https://www.freetogame.com/api/game/{game_id}")
+    response = requests.get(f"https://www.freetogame.com/api/game?id={game_id}")
     game = response.json()
 
     information = {
         'name': game['title'],
         'description': game['description'],
-        'url': game['freetogame_profileurl'],
         'thumbnail': game['thumbnail'],
         'genre': game['genre'],
         'publisher': game['publisher'],
         'freetogame_profile_url': game['game_url']   #Thats what it's named in the API so keep it game_url
     }
 
-    return render_template("description.html", information=information)
+    return render_template("information.html", information=information)
 
 if __name__ == '__main__':
     app.run(debug=True)
