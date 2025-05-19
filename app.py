53d6aefdfd6b72ce8454a2866d337f504fead1f9
JohnnyZhang7 <zjohnnyzhang@gmail.com>
2025-05-19 09:25:54 -0400

    5/19/25 fix it printing the same genre for each

--- app.py
+++ app.py
@@ -8,18 +8,26 @@ def index():
     response = requests.get("https://www.freetogame.com/api/games?platform=pc")
     data = response.json()[:150]       #My API doesn't have ?limit as a parameter
 
-    games = []
+    genres = []
+    genre_groups = {}
 
     for game in data:
-        games.append({
-        'id': game['id'],
-        'name': game['title'],
-        'thumbnail': game['thumbnail'],
-        'genre': game['genre'],
-        'publisher': game['publisher']
-        })
+        if game['genre'] not in genres:
+            genres.append(game['genre'])    #Stores the individual genres so that I can sort them later
+            genre_groups[game['genre']] = []
 
-    return render_template("index.html", games=games)
+        game_info = {
+            'id': game['id'],
+            'name': game['title'],
+            'thumbnail': game['thumbnail'],
+            'genre': game['genre'],
+            'publisher': game['publisher']
+        }
+        genre_groups[game['genre']].append(game_info)
+
+
+    return render_template("index.html", genre_groups=genre_groups)
+
 
 @app.route("/game/<int:game_id>")
 def description_link(game_id):
@@ -27,7 +35,7 @@ def description_link(game_id):
     game = response.json()
     try:
         information = {
-        'chaojie' : game['chaoejie'],
+        #'chaojie' : game['chaojie'],    #If I add chaojie into the information, it isn't part of the API so returns with an error
         'name': game['title'],
         'description': game['description'],
         'thumbnail': game['thumbnail'],
